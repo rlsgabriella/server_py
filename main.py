a2f0d424c4ad835724efb41a538964e20e22cf0a
Gabriella Rodrigues <rlsgabriella@gmail.com>
2025-09-12 04:55:00 +0000

    testando codigo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import fastapi
+from fastapi import FastAPI
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+openai = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 app = FastAPI()
 
 @app.get("/")
