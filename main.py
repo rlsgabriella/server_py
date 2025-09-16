7dc7303f64805ce58744d38c3013f6fa75075068
Gabriella Rodrigues <rlsgabriella@gmail.com>
2025-09-15 21:37:52 -0300

    enviando código backend

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
-from fastapi import fastapi
+from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+from google import genai
+import json
+
+load_dotenv()  # carrega o .env
+
+API_KEY = os.getenv("GOOGLE_API_KEY")
+client = genai.Client(api_key=API_KEY)
 
 app = FastAPI()
 
+# Habilita CORS para testar localmente
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # teste local
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
-    return {"message" : "Hello word!"}
+    return {"message": "API de Classificação de Emails com Gemini"}
+
+@app.post("/classify/")
+async def classify_email(text: str = Form(...)):
+    prompt = f"""
+    Você é um classificador de emails.
+    Analise o email abaixo e responda **apenas em JSON** com:
+    {{
+        "categoria": "Produtivo ou Improdutivo",
+        "resposta": "Mensagem curta sugerida"
+    }}
+    
+    Email:
+    {text}
+    """
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt 
+    )
+
+    try:
+        result = json.loads(response.text.strip())
+    except:
+        result = {"categoria": "Desconhecido", "resposta": response.text.strip()}
+
+    return {"email": text, "classificacao": result}
